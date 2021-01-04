1d82769c82b4902f62f531de3820168a60f241b4
tsuyo-itai <suka_sudeilu@yahoo.co.jp>
2021-01-04 23:33:04 +0900

    debug用ページ削除

--- grandmenu/myapp/urls.py
+++ grandmenu/myapp/urls.py
@@ -6,8 +6,6 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('debug_storeinfo/', views.debug_storeinfo, name='debug_storeinfo'),    #debug
-    path('debug_storeinfolist/', views.debug_storeinfolist, name='debug_storeinfolist'),    #debug
     path('store_setting/', views.store_setting, name='store_setting'),
     path('store_mypage/', views.store_mypage, name='store_mypage'),
     path('store_mypage/edit/', views.store_mypage_edit, name='store_mypage_edit'),
--- grandmenu/myapp/views.py
+++ grandmenu/myapp/views.py
@@ -10,26 +10,6 @@ import json
 class Home(generic.TemplateView):
     template_name = 'myapp/home.html'
 
-def debug_storeinfo(request):
-    params = {'message': '', 'form': None}
-    if request.method == 'POST':
-        form = StoreInfoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('myapp:debug_storeinfolist')
-        else:
-            params['message'] = '再入力して下さい'
-            params['form'] = form
-    else:
-        params['form'] = StoreInfoForm()
-    return render(request, 'myapp/debug_storeinfo.html', params)
- 
- 
-def debug_storeinfolist(request):
-    data = StoreInfo.objects.all()
-    params = {'message': '店情報の一覧', 'data': data}
-    return render(request, 'myapp/debug_storeinfolist.html', params)
-
 @login_required
 def store_setting(request):
     params = {'message': '', 'form': None}
